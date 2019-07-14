d200c886d5449f61c7023cc349bf966b55c8cdb9
David Uriel R <davrodriguez@belatrixsf.com>
2019-07-13 22:21:52 -0500

    Added BrowserRouter component. Now the options > Link update the url correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import { MemoryRouter as Router } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 import { routes } from './routes'
 
 function App() {
 	return (
 		<Router>
-			{routes()}
+			<BrowserRouter>
+				{routes()}
+			</BrowserRouter>
 		</Router>
 	)
 }
