03eb8936b99657a88b6e5cfdc90629660bc06a11
davrodriguez <davrodriguez@belatrixsf.com>
2019-10-31 09:24:31 -0500

    Added call to API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
-import { MemoryRouter as Router } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
+import React, { useState } from "react";
+import "./App.css";
+import { MemoryRouter as Router } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
-import { routes } from './routes'
+import { routes } from "./routes";
 
 function App() {
-	return (
-		<Router>
-			<BrowserRouter>
-				{routes()}
-			</BrowserRouter>
-		</Router>
-	)
+  const [apiResponse, setApiResponse] = useState({});
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => setApiResponse({ apiResponse: res }));
+  };
+  callAPI();
+  return (
+    <>
+      <Router>
+        <BrowserRouter>{routes()}</BrowserRouter>
+      </Router>
+      {(() => console.log(apiResponse))()}
+    </>
+  );
 }
 
 export default App;
